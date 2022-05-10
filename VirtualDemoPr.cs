3cb47e62f1516a2809f0561842a76f429b81efc9
RanjithChe55 <102515797+RanjithChe55@users.noreply.github.com>
2022-05-10 22:44:25 +0530

    Add files via upload

---
+++ VirtualDemoPr.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+
+namespace NewKeyDemo
+{
+    class Baseclass
+    {
+        public virtual void display()
+        {
+            Console.WriteLine("Base class Executed..");
+            Console.ReadLine();
+        }
+    }
+    class Derivedclass : Baseclass
+    {
+        public override void display()
+        {
+            Console.WriteLine("First Derived Class Override function");
+            Console.ReadLine();
+
+        }
+    }
+    class Derivedclass2 : Baseclass
+    {
+        public new void display()
+        {
+            Console.WriteLine("Second Derived Class function using new keyword");
+            Console.ReadLine();
+
+
+
+        }
+    }
+    class Mainclass
+    {
+        public static void Main()
+        {
+            
+            Console.WriteLine("Override Function");
+            Baseclass one = new Derivedclass();
+            one.display();
+            Console.WriteLine("Using new keyword");
+            Baseclass two = new Derivedclass2();
+            two.display();
+        }
+    }
+}
